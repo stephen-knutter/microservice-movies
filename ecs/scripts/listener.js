e177ce4de6aa87c8bd2693803df2f8dbcb7c89c0
stephen Knutter <stephenknutter@stephens-MacBook-Air.local>
2018-07-14 08:03:30 -0600

    fix

--- ecs/scripts/listener.js
+++ ecs/scripts/listener.js
@@ -12,7 +12,7 @@ const LOAD_BALANCER_ARN = process.env.LOAD_BALANCER_ARN;
 
 // config
 
-AWS.config = new AWS.config();
+AWS.config = new AWS.Config();
 AWS.config.accessKeyId = AWS_ACCESS_KEY_ID;
 AWS.config.secretAccessKey = AWS_SECRET_ACCESS_KEY;
 AWS.config.region = AWS_CONFIG_REGION;
